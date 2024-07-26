fc799a1917f738900d2f6da9f1e975709ddcf54a
zondiMaqina <zondimaqina0@gmail.com>
2024-07-26 15:28:13 +0200

    ad API reading

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,9 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
 puts 'Event Manager Initialzed'
 
 def clean_zipcode(zipcode)
@@ -22,5 +27,16 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials  
+  rescue
+    puts 'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+  
+  puts "#{name} #{zipcode} #{legislators}"
 end
