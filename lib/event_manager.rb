0bf0b345e5c6946cc717c176e83dd8a55271cdcd
zondiMaqina <zondimaqina0@gmail.com>
2024-07-26 10:17:24 +0200

    print lines of csv file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,3 +2,9 @@ puts 'Event Manager Initialzed'
 
 persons_details = File.read('/home/zondi-maqina/ruby_projects/event_manager/event_attendees.csv')
 puts persons_details
+
+lines = File.readlines('/home/zondi-maqina/ruby_projects/event_manager/event_attendees.csv')
+lines.each do |line|
+  puts line
+end
+
