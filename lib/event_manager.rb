7310d71c452358945402ea97e85135a517d16cef
zondiMaqina <zondimaqina0@gmail.com>
2024-07-26 11:57:11 +0200

    move  file opening class

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,15 +1,7 @@
 require 'csv'
 puts 'Event Manager Initialzed'
 
-contents = CSV.open('/home/zondi-maqina/ruby_projects/event_manager/event_attendees.csv',
- headers: true,
- header_converters: :symbol
- )
-
-contents.each do |row|
-  name = row[:first_name]
-  zipcode = row[:zipcode]
-
+def clean_zipcode(zipcode)
   if zipcode.nil?
     zipcode = '00000'
   elsif zipcode.length < 5
@@ -19,5 +11,16 @@ contents.each do |row|
   else
     zipcode
   end
+end
+
+contents = CSV.open('/home/zondi-maqina/ruby_projects/event_manager/event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+
   puts "#{name} #{zipcode}"
 end
