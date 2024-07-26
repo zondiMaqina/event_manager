5d297be32432dfacb184a6600e9e260781050e81
zondiMaqina <zondimaqina0@gmail.com>
2024-07-26 11:00:23 +0200

    fetch name of each attendee

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,11 +1,14 @@
 puts 'Event Manager Initialzed'
 
 persons_details = File.read('/home/zondi-maqina/ruby_projects/event_manager/event_attendees.csv')
-puts persons_details
 
 lines = File.readlines('/home/zondi-maqina/ruby_projects/event_manager/event_attendees.csv')
+row_index = 0
 lines.each do |line|
+  row_index = (row_index + 1)
+  next if row_index == 1
   column = line.split(',')
-  puts column
+  name = column[2]
+  puts name
 end
 
