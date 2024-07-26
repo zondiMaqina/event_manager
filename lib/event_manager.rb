bd02d213600388508099f0de8129a6310a17c57a
zondiMaqina <zondimaqina0@gmail.com>
2024-07-26 11:09:43 +0200

    use CSV parser

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,14 +1,8 @@
+require 'csv'
 puts 'Event Manager Initialzed'
 
-persons_details = File.read('/home/zondi-maqina/ruby_projects/event_manager/event_attendees.csv')
-
-lines = File.readlines('/home/zondi-maqina/ruby_projects/event_manager/event_attendees.csv')
-row_index = 0
-lines.each do |line|
-  row_index = (row_index + 1)
-  next if row_index == 1
-  column = line.split(',')
-  name = column[2]
+contents = CSV.open('/home/zondi-maqina/ruby_projects/event_manager/event_attendees.csv', headers: true)
+contents.each do |row|
+  name = row[2]
   puts name
 end
-
